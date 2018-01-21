4d0412b9d35204ddeac012c20c9b8fd19d31c552
Boese <boesej@oregonstate.edu>
2018-01-21 10:02:10 -0600

    Added refactoredCards files

---
+++ dominion/refactoredCards.c
@@ -0,0 +1,9 @@
+/***************************************************
+ *Project name: Dominion
+ *File name: refactoredCards.c
+ *Author: Jonathan Boese
+ *Date: 1/19/2018
+ *Description:
+****************************************************/
+
+
---
+++ dominion/refactoredCards.h
@@ -0,0 +1,24 @@
+/***************************************************
+ *Project name: Dominion
+ *File name: refactoredCards.h
+ *Author: Jonathan Boese
+ *Date: 1/19/2018
+ *Description:
+****************************************************/
+#include "dominion_helpers.h"
+
+
+#ifndef CS362_004_W2018_REFACTOREDCARDS_H
+#define CS362_004_W2018_REFACTOREDCARDS_H
+
+void refactoredSteward();
+
+void refactoredSmithy();
+
+void refactoredAdventurer();
+
+void refactoredAmbassador();
+
+void refactoredVillage();
+
+#endif //CS362_004_W2018_REFACTOREDCARDS_H
