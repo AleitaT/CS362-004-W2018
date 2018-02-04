b57cb3681cf1f718c237d7f07aad2319815c1495
AleitaT <traina@oregonstate.edu>
2018-02-04 12:56:00 -0800

    cleanup on tests and coverage

--- projects/traina/dominion/cardtest1.c
+++ projects/traina/dominion/cardtest1.c
@@ -12,6 +12,8 @@
 
 int main () {
 
+   printf("\n **** CARDTEST1: ADVENTURER **** \n");
+
    int kingdomCards[10] = {adventurer, embargo, village, minion, mine, cutpurse, remodel, tribute, smithy, council_room};
    int i = 0;
    int handPos = 0;
@@ -24,72 +26,83 @@ int main () {
    int pass = 1;
 
    struct gameState g, tester;
-
    initializeGame(2, kingdomCards, 100, &g);
-
-   printf("\n **** CARDTEST1: ADVENTURER **** \n");
-
+   memcpy(&tester, &g, sizeof(struct gameState));
+   cardEffect(adventurer, choice1, choice2, choice3, &tester, handPos, &bonus);
 
    /**************************************************************
    * Handcount should go up by 2 for player who plays card
    ****************************************************************/
-   memcpy(&tester, &g, sizeof(struct gameState));
-
-   cardEffect(adventurer, choice1, choice2, choice3, &tester, handPos, &bonus);
-
-   if (tester.handCount[0] != g.handCount[0] + drawn - discard) {
+   int startHandCount = tester.handCount[0];
+   int endHandCount = g.handCount[0] + drawn - discard;
+   if ( startHandCount != endHandCount) {
       pass = 0;
-      printf("\tFailed: Hand count after playing adventurer was not correct. Expected %d, got %d\n", g.handCount[0] + drawn - discard, tester.handCount[0]);
-   }
-   else {
-      printf("\tSuccess: Correct hand count after playing adventurer (+2)\n");
+      printf("\tFailed: Expected hand count after adventurer card to be %d, got %d\n", endHandCount, startHandCount);
+   } else {
+      printf("\tSuccess: Hand count after playing adventurer is correct(+2)\n");
    }
    
    /*********************************************************
    * Check other players state 
    *********************************************************/
-   if (tester.handCount[1] != g.handCount[1]) {
+   
+   int componentHandStart = tester.handCount[1];
+   int componentHandEnd = g.handCount[1];
+
+   if ( componentHandStart != componentHandEnd ) {
       pass = 0;
-      printf("\tFailed: Player 2's handcount changed after player 1 played adventurer. Expected %d, got %d\n", g.handCount[1], tester.handCount[1]);
-   }
-   else {
+      printf("\tFailed: Player 2's handcount changed after player 1 played adventurer. Expected %d, got %d\n", componentHandEnd, componentHandStart);
+   } else {
       printf("\tSuccess: Player 2's handcount unchanged after player 1 played adventurer\n");
    }
 
-   if (tester.deckCount[1] != g.deckCount[1]) {
-      pass = 0;
-      printf("\tFailed: Player 2's deckcount changed after player 1 played adventurer. Expected %d, got %d\n", g.deckCount[1], tester.deckCount[1]);
-   }
-   else {
-      printf("\tSuccess: Player 2's deckcount unchanged after player 1 played adventurer\n");
-   }
 
-   if (scoreFor(1, &tester) != scoreFor(1, &g)) {
+   int componentScoreStart = scoreFor(1, &tester);
+   int componentScoreEnd = scoreFor(1, &g);
+
+   if (componentScoreStart != componentScoreEnd) {
       pass = 0;
-      printf("\tFailed: Score for player 2 changed when player 1 played adventurer. Expected %d, got %d\n", scoreFor(1, &g), scoreFor(1, &tester));
-   }
-   else {
+      printf("\tFailed: Score for player 2 changed when player 1 played adventurer. Expected %d, got %d\n", componentScoreEnd, componentScoreStart);
+   } else {
       printf("\tSuccess: Player 2's score unchanged after player 1 played adventurer\n");
    }
 
+   /* check component card pile after playing adventurer */
+ 
+   int componentDeckStart = tester.deckCount[1];
+   int componentDeckEnd = g.deckCount[1];
+   
+   if (componentDeckStart != componentDeckEnd) {
+      pass = 0;
+      printf("\tFailed: Player 2's deckcount changed after player 1 played adventurer. Expected %d, got %d\n", componentDeckEnd, componentDeckStart);
+   } else {
+      printf("\tSuccess: Player 2's deckcount unchanged after player 1 played adventurer\n");
+   }
 
    /**********************************************************
-   * deckcount should goe down by 2 for the 2 cards drawn
+   * deckcount should go down by 2 for the 2 cards drawn
    **********************************************************/
-   if (tester.deckCount[0] != g.deckCount[0] - drawn) {
+
+   int playerDeckCount = tester.deckCount[0];
+   int playerDeckCountDraw = g.deckCount[0] - drawn;
+   if (playerDeckCount != playerDeckCountDraw) {
       pass = 0;
-      printf("\tFailed: Deck count after playing adventurer was not correct. Expected %d, got %d\n", g.deckCount[0] - drawn, tester.deckCount[0]);
+      printf("\tFailed: Deck count after playing adventurer was not correct. Expected %d, got %d\n", playerDeckCountDraw, playerDeckCount);
    }
    else {
       printf("\tSuccess: Correct deck count after playing adventurer (-2)\n");
    }
 
-   //Test if the score changed - it should be the same as before
-   if (scoreFor(0, &tester) != scoreFor(0, &g)) {
+
+   /**********************************************************
+   * score should be the same as before
+   **********************************************************/
+   int scoreBefore = scoreFor(0, &tester);
+   int scoreAfter = scoreFor(0, &g);
+   if (scoreBefore != scoreAfter) {
       pass = 0;
-      printf("\tFailed: Score after playing adventurer was not correct. Expected %d, got %d\n", scoreFor(0, &g), scoreFor(0, &tester));
-   }
-   else {
+      printf("\tFailed: Score after playing adventurer was not correct. Expected %d, got %d\n", scoreAfter, scoreBefore);
+   } else {
       printf("\tSuccess: Correct score after playing adventurer (no change)\n");
    }
 
@@ -97,60 +110,35 @@ int main () {
    * Kingdom Card Piles
    *******************************/
    for (i = 0; i < 10; i++) {
-
-      if (tester.supplyCount[kingdomCards[i]] != g.supplyCount[kingdomCards[i]]) {
-	 pass = 0;
-	 printf("\tFailed: Kingdom card pile #%d did not contain the same number of cards\n", i);
-      }
-      else {
-	 printf("\tSuccess: Kingdom card pile #%d contained the same number of cards as before\n", i);
+      int testerKindgdom = tester.supplyCount[kingdomCards[i]];
+      int gKingdom = g.supplyCount[kingdomCards[i]];
+      if (  testerKindgdom!= gKingdom ) {
+	     pass = 0;
+	     printf("\tFailed: Kingdom card pile #%d did not contain the same number of cards\n", i);
+      } else {
+	     printf("\tSuccess: Kingdom card pile #%d contained the same number of cards as before\n", i);
       }
    }
 
-   /*************************************
+   /******************************************************************
    * Tests first two cards taken 
-   **************************************/
-   if (tester.hand[0][tester.handCount[0]-1] != copper && tester.hand[0][tester.handCount[0]-1] != silver && tester.hand[0][tester.handCount[0]-1] != gold) {
+   *****************************************************************/
+
+   int firstCard = tester.hand[0][tester.handCount[0]-1];
+   int secondCard = tester.hand[0][tester.handCount[0]-2];
+
+   if (firstCard != copper && firstCard != silver && firstCard != gold) {
       pass = 0;
       printf("\tFailed: First card taken was not a treasure card\n");
-   }
-   else {
+   } else {
       printf("\tSuccess: First card = treasure card \n");
    }
    
-   if (tester.hand[0][tester.handCount[0]-2] != copper && tester.hand[0][tester.handCount[0]-2] != silver && tester.hand[0][tester.handCount[0]-2] != gold) {
+   if (secondCard != copper && secondCard != silver && secondCard != gold) {
       pass = 0;
       printf("\tFailed: Second card taken was not a treasure card\n");
-   }
-   else {
+   } else {
       printf("\tSuccess: Second card = treasure card\n");
-   }
-
-   /**************************************************
-   * Number of victory cards piles should be the same
-   ***************************************************/
-   if (tester.supplyCount[estate] != g.supplyCount[estate]) {
-      pass = 0;
-      printf("\tFailed: Number of estate cards differed. Expected %d, got %d\n", g.supplyCount[estate], tester.supplyCount[estate]);
-   }
-   else {
-      printf("\tSuccess: Number of estate cards the same after playing adventurer\n");
-   }
-
-   if (tester.supplyCount[duchy] != g.supplyCount[duchy]) {
-      pass = 0;
-      printf("\tFailed: Number of duchy cards differed. Expected %d, got %d\n", g.supplyCount[duchy], tester.supplyCount[duchy]);
-   }
-   else {
-      printf("\tSuccess: Number of duchy cards the same after playing adventurer\n");
-   }
-
-   if (tester.supplyCount[province] != g.supplyCount[province]) {
-      pass = 0;
-      printf("\tFailed: Number of province cards differed. Expected %d, got %d\n", g.supplyCount[province], tester.supplyCount[province]);
-   }
-   else {
-      printf("\tSuccess: Number of province cards the same after playing adventurer\n");
    }
 
    /***********************************
--- projects/traina/dominion/unittest1.c
+++ projects/traina/dominion/unittest1.c
@@ -1,5 +1,5 @@
 /************************************************
-* Unit Testing for getCost function
+* Unit Testing for getCost() function
 *************************************************/
 
 #include <stdlib.h>
@@ -12,16 +12,268 @@
 
 int main(int argc, char** argv){
 
-  int a = getCost(0);
-  assert(a == 0);
-  int b = getCost(adventurer);
-  assert(b == 6);
-  int c = getCost(village);
-  assert(c == 3);
-  int d = getCost(baron);
-  assert(d == 4);
-	printf("**** unittest1 getCost() unit test passed **** \n");
-	return 0;
+  printf("\n **** unittest1 getCost() **** \n");
+
+  int i = 0;
+  int cost = 0;
+  int found = 0;
+
+  for( i = 0; i < 30; i++) {
+
+    cost = getCost(i);
+
+    switch(i) {
+
+      case curse:
+        if (cost == 0) {
+          printf("\tSuccess: returned cost of 0 for curse card.\n");
+        } else {
+          found = 1;
+          printf("\tFailed: curse card expected cost of 0 received %d\n", cost);
+        }
+        break;
+
+      case estate:
+        if (cost == 2) {
+          printf("\tSuccess: returned cost of %d for estate card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: estate card expected cost of 2 received %d\n", cost);
+        }
+        break;
+
+      case duchy:
+        if (cost == 5) {
+          printf("\tSuccess: returned cost of %d for duchy card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: duchy card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case province:
+        if (cost == 8) {
+          printf("\tSuccess: returned cost of %d for privince card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: privince card expected cost of 8 received %d\n", cost);
+        }
+        break;
+
+      case copper:
+          if (cost == 0) {
+          printf("\tSuccess: returned cost of %d for 0 card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: copper card expected cost of 0 received %d\n", cost);
+        }
+        break;
+
+      case silver:
+        if (cost == 3) {
+          printf("\tSuccess: returned cost of %d for silver card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: silver card expected cost of 3 received %d\n", cost);
+        }
+        break;
+
+      case gold:
+        if (cost == 6) {
+          printf("\tSuccess: returned cost of %d for gold card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: gold card expected cost of 6 received %d\n", cost);
+        }
+        break;
+
+      case adventurer:
+        if (cost == 6) {
+          printf("\tSuccess: returned cost of %d for adventurer card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: adventurer card expected cost of 6 received %d\n", cost);
+        }
+        break;
+
+      case council_room:
+        if (cost == 5) {
+          printf("\tSuccess: returned cost of %d for council_room card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: council room card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case feast:
+        if ( cost == 4 ){
+          printf("\tSuccess: returned cost of %d for feast card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: feast card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case gardens:
+        if (cost == 4) {
+          printf("\tSuccess: returned cost of %d for gardens card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: cardens card expected cost of 4 received %d\n", cost);
+        }
+        break;
+        
+      case mine:
+        if(cost == 5) {
+          printf("\tSuccess: returned cost of %d for mine card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: mine card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case remodel:
+        if (cost== 4){
+          printf("\tSuccess: returned cost of %d for remodel card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: remodel card expected cost of 4 received %d\n", cost);
+        }
+        break;
+  
+      case smithy:
+        if ( cost == 4){
+          printf("\tSuccess: returned cost of %d for smithy card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: smithy card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case village:
+        if ( cost == 3) {
+          printf("\tSuccess: returned cost of %d for village card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: council room card expected cost of 3 received %d\n", cost);
+        }
+        break;
+
+      case baron:
+        if ( cost == 4) {
+          printf("\tSuccess: returned cost of %d for baron card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: baron card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case great_hall:
+        if ( cost == 3) {
+          printf("\tSuccess: returned cost of %d for great_hall card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: great hall card expected cost of 3 received %d\n", cost);
+        }
+        break;
+
+      case minion:
+        if ( cost == 5) {
+          printf("\tSuccess: returned cost of %d for minion card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: minion card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case steward:
+        if ( cost == 3) {
+          printf("\tSuccess: returned cost of %d for steward card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: steward card expected cost of 3 received %d\n", cost);
+        }
+        break;
+
+      case tribute:
+        if ( cost == 5) {
+          printf("\tSuccess: returned cost of %d for tribute card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: tribute card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case ambassador:
+        if ( cost == 3) {
+          printf("\tSuccess: returned cost of %d for ambassador card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: ambassador card expected cost of 3 received %d\n", cost);
+        }
+        break;
+
+      case cutpurse:
+        if(cost == 4){
+          printf("\tSuccess: returned cost of %d for cutpurse card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: cutpurse card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case embargo: 
+        if (cost == 2){
+          printf("\tSuccess: returned cost of %d for embargo card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: embargo card expected cost of 2 received %d\n", cost);
+        }
+        break;
+
+      case outpost:
+        if( cost == 5) {
+          printf("\tSuccess: returned cost of %d for outpost card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: outpost card expected cost of 5 received %d\n", cost);
+        }
+        break;
+
+      case salvager:
+        if ( cost == 4) {
+          printf("\tSuccess: returned cost of %d for salvager card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: salvager card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case sea_hag:
+        if(cost == 4) {
+          printf("\tSuccess: returned cost of %d for sea_hag card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: sea_hag card expected cost of 4 received %d\n", cost);
+        }
+        break;
+
+      case treasure_map:
+        if(cost == 4) {
+          printf("\tSuccess: returned cost of %d for treasure_map card\n", cost);
+        } else {
+          found = 1;
+          printf("\tFailed: treasure map expected cost of 4 received %d\n", cost);
+        }
+        break;
+    }
+  }
+  if (found) {
+    printf("**** unittest1 getCost() found at least (1) error **** \n\n");
+  } else {
+  printf("**** unittest1 getCost() unittest1 passed with 0 errors **** \n\n");
+  }
+  return 0;
 }
 
 
